986cc35c91c3fd905d45115529336759a4d3ec98
Leonard Velásquez <leonard.avp@gmail.com>
2022-08-09 18:29:03 -0400

    Eliminación de valores privados

--- src/app/db.ts
+++ src/app/db.ts
@@ -9,34 +9,7 @@ export class ContactoDB extends Dexie{
         this.version(3).stores({
             contactos: '++id'
         })
-        this.on('populate',()=>this.populate())
     }
-
-    async populate() {        
-        await db.contactos.bulkAdd([
-          {
-            nombre:'Leonard Alejandro',
-            apellido:'Velásquez Pinto',
-            cedula:28724030,
-            correo: 'leonard.avp@gmail.com',
-            telefono: 4147722057,
-          },
-          {
-            nombre:'Yajaira Mercedes',
-            apellido:'Pinto Rodriguez',
-            cedula:11213492,
-            correo: 'yajairampr@gmail.com',
-            telefono: 4243543468,
-          },
-          {
-            nombre:'Leonardo Habacuc',
-            apellido:'Velásquez Parra',
-            cedula:13456987,
-            correo: 'leonard.hvp@gmail.com',
-            telefono:412589674,
-          },
-        ]);
-      }
 }
 
 export const db = new ContactoDB()
