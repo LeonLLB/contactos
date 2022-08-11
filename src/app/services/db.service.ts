cca137dafc9747e1c43df0098b67c8078ec7caa8
Leonard Velásquez <leonard.avp@gmail.com>
2022-08-11 15:04:44 -0400

    Logica CRUD contactos

--- src/app/services/db.service.ts
+++ src/app/services/db.service.ts
@@ -1,17 +1,30 @@
 import { Injectable } from '@angular/core';
-import { Contacto } from '../interfaces/contacto.interface';
 import {db} from 'src/app/db'
 import { liveQuery } from 'dexie';
+import { Contacto } from '../interfaces/contacto.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DBService {
-
-  add(){}
-
+  
   getLiveQuery(){
     return liveQuery(()=>db.contactos.toArray())
+  }
+  
+  addContacto(contacto: Contacto): Promise<boolean>{
+    return db.contactos.add(contacto)
+    .then(data=>console.log(data))
+    .then(()=>true)
+  }
+
+  updateContacto(contacto:Contacto): Promise<boolean>{
+    return db.contactos.update(contacto.id!,contacto)
+    .then(data=>data===1)
+  }
+
+  deleteContacto(id:number): Promise<void>{
+    return db.contactos.delete(id)
   }
 
 }
